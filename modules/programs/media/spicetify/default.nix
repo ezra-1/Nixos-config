2f7df49e1e152e8cdf8ad8366381a95484515a4b
ezra <ezralawrence02@gmail.com>
2025-11-01 13:15:37 -0500

    🧊 feat: configure Spicetify with theme, extensions, and custom apps
    - Enables Spicetify via Home Manager
    - Sets lucid theme and empty color scheme
    - Activates extensions: shuffle, keyboardShortcut, copyLyrics
    - Adds custom apps: marketplace, lyricsPlus
    - Allows unfree Spotify packages

---
+++ modules/programs/media/spicetify/default.nix
@@ -0,0 +1,37 @@
+{ config, lib, pkgs, inputs, ... }:
+
+let
+  spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.system};
+in
+{
+  # ------------------------------------------------------
+  # Allow unfree Spotify
+  # ------------------------------------------------------
+  nixpkgs.config.allowUnfreePredicate = pkg:
+    lib.hasPrefix "spotify" (lib.getName pkg);
+
+  # ------------------------------------------------------
+  # Spicetify configuration
+  # ------------------------------------------------------
+  programs.spicetify = {
+    enable = true;
+
+    # Theme
+    theme = spicePkgs.themes.lucid;
+    colorScheme = "";
+
+    # Extensions
+    enabledExtensions = with spicePkgs.extensions; [
+      shuffle
+      keyboardShortcut
+      copyLyrics
+    ];
+
+    # Custom apps
+    enabledCustomApps = with spicePkgs.apps; [
+      marketplace
+      lyricsPlus
+    ];
+  };
+}
+
